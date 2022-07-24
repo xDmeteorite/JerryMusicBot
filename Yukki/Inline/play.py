ca5e0377381db6d464e2c71acf141b2f20859997
Meteorite✨ <109398535+xDmeteorite@users.noreply.github.com>
2022-07-24 22:44:52 +0530

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -227,7 +227,7 @@ def timer_markup(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{total_time} ▒▒JerryMusicBot▒▒ {current_time}",
+                text=f"{total_time} ▃▄▂▄▇▅▃▄▂▄▇▅▃▅▁ {current_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
@@ -254,7 +254,7 @@ def audio_markup(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{total_time} ------------------ {current_time}",
+                text=f"{total_time} ▃▄▂▄▇▅▃▄▂▄▇▅▃▅▁ {current_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
@@ -273,7 +273,7 @@ def audio_timer_markup_start(videoid, user_id, current_time, total_time):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{total_time} ------------------ {current_time}",
+                text=f"{total_time} ▃▄▂▄▇▅▃▄▂▄▇▅▃▅▁ {current_time}",
                 callback_data=f"timer_checkup_markup {videoid}|{user_id}",
             )
         ],
